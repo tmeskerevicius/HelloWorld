2b5cfb5be48d887b65e24ead7804d5f83f8303e8
tmeskerevicius <9na9GL32PD>
2020-09-11 13:24:01 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+print("Don")
+print("By the way HELLO WORLD!!!")
+price = 20
+price = 50
+print (price)
+if_no_price = False
+print(if_no_price)
+name = "John"
+age= 30
+is_new_patient = True
+name = input("What is your name \n")
+birth_year = input("Birth year: ")
+age = 2020 - int(birth_year)
+
+print (name +" is "+ str(age) + " years old")
+print ("Hello  " + name)
+
+colour = input(name + ", what is your favourite colour? \n")
+
+print(name+"'s favourite coulour is "+colour)
+
+weight_inpounds=input("What's your weight in pounds?  ")
+weight_inkilos = 0.45 * int(weight_inpounds)
+print(name +"'s weight in kilos is  " + str(weight_inkilos) + "kg")
+print('"start" course')
