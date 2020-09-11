2dd26f2f33fa96a091fd46ede2b80d1fef650c5c
tmeskerevicius <9na9gl32pd>
2020-09-11 16:09:44 +0100

    string operations

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-cource = '"start" course'
-print (cource[0:-5])
-print (cource[0:])
-print (cource[0:1])
-print (cource[0:0]) # empty line from zero up to and not including zero
-print (cource[1:1]) # empty line from 1 up to and not including 1
-print (cource[0:5])
-
+cource = '"Start" Course'
+print(cource[0:-5])
+print(cource[0:])
+print(cource[0:1])
+print(cource[0:0]) # empty line from zero up to and not including zero
+print(cource[1:1]) # empty line from 1 up to and not including 1
+print(cource[0:5])
+name = "Lucy"
+print (len(cource))
+text = f'print this text {cource} and then print name {name}'
+print(text)
+print(cource.find("o"))
+print(cource.lower())
+print(cource.upper())
+print(cource.find("d"))
