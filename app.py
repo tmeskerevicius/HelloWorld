e155f4d29ec237f552d06fe6230f4c871bc9ae30
tmeskerevicius <9na9GL32PD>
2020-09-11 14:15:31 +0100

    2nd commit

--- app.py
+++ app.py
@@ -1,25 +1,8 @@
-print("Don")
-print("By the way HELLO WORLD!!!")
-price = 20
-price = 50
-print (price)
-if_no_price = False
-print(if_no_price)
-name = "John"
-age= 30
-is_new_patient = True
-name = input("What is your name \n")
-birth_year = input("Birth year: ")
-age = 2020 - int(birth_year)
+cource = '"start" course'
+print (cource[0:-5])
+print (cource[0:])
+print (cource[0:1])
+print (cource[0:0]) # empty line from zero up to and not including zero
+print (cource[1:1]) # empty line from 1 up to and not including 1
+print (cource[0:5])
 
-print (name +" is "+ str(age) + " years old")
-print ("Hello  " + name)
-
-colour = input(name + ", what is your favourite colour? \n")
-
-print(name+"'s favourite coulour is "+colour)
-
-weight_inpounds=input("What's your weight in pounds?  ")
-weight_inkilos = 0.45 * int(weight_inpounds)
-print(name +"'s weight in kilos is  " + str(weight_inkilos) + "kg")
-print('"start" course')
