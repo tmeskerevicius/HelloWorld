24bb4ad9fa46ae5a9d14927cb8b1ec13cc43dadb
tmeskerevicius <9na9GL32PD>
2020-09-13 16:36:02 +0100

    string operations2

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-cource = '"Start" Course'
-print(cource[0:-5])
-print(cource[0:])
-print(cource[0:1])
-print(cource[0:0]) # empty line from zero up to and not including zero
-print(cource[1:1]) # empty line from 1 up to and not including 1
-print(cource[0:5])
-name = "Lucy"
-print (len(cource))
-text = f'print this text {cource} and then print name {name}'
-print(text)
-print(cource.find("o"))
-print(cource.lower())
-print(cource.upper())
-print(cource.find("d"))
+
+
+def emoji_picker(message):
+    words = message.split(" ")
+    emojis = {
+        ":)" : "😊",
+        ":(" : "😒",
+        ":D":"😁",
+        ":/":"🤔",
+        ";)": "😉" ,
+        ":|":"😐️"
+
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word,word) + " "
+    return output
+
+
+info = input ("-->")
+
+print(emoji_picker(info))
