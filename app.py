d2df7cdda75241afdb373a1b40debe2a041fb2ae
tmeskerevicius <9na9GL32PD>
2020-09-15 16:36:24 +0100

    Going further

--- app.py
+++ app.py
@@ -1,22 +1,4 @@
 
+age = int(input("Age "))
+print(age)
 
-def emoji_picker(message):
-    words = message.split(" ")
-    emojis = {
-        ":)" : "😊",
-        ":(" : "😒",
-        ":D":"😁",
-        ":/":"🤔",
-        ";)": "😉" ,
-        ":|":"😐️"
-
-    }
-    output = ""
-    for word in words:
-        output += emojis.get(word,word) + " "
-    return output
-
-
-info = input ("-->")
-
-print(emoji_picker(info))
