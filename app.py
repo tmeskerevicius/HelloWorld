aa9a6d149cfba7b5042f08725a0c4fa5e2e490c8
tmeskerevicius <9na9GL32PD>
2020-09-16 15:43:03 +0100

    classes

--- app.py
+++ app.py
@@ -1,4 +1,39 @@
+class Mammal:
+    def __init__(self, name):
+        self.name = name  # can be any type no type checking
 
-age = int(input("Age "))
-print(age)
+    def run(self):
+        print(f"{self.name} runs")
 
+    def eat(self):
+        print(f"{self.name} eats")
+
+    def jump(self):
+        print(f"{self.name} jumps")
+
+
+class Dog(Mammal):
+    def bark(self):
+        print(f"{self.name} barks")
+
+
+class Cat(Mammal):
+    def meow(self):
+        print(f"{self.name} meows")
+
+
+dogJoe = Dog("Joe")
+catRee = Cat("Ree")
+dogJoe.food = "pedegree"
+catRee.food = "whiskas"
+dogJoe.age = 2
+catRee.age = 3
+dogJoe.bark()
+catRee.meow()
+
+catRee.run()
+dogJoe.jump()
+print(catRee.food)
+print(catRee.age)
+print(dogJoe.food)
+print(dogJoe.age)
